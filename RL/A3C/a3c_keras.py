07d5407b7f16d7ec5d64d49fa9d71ee21db6fffb
louis <yangzhikai@cug.edu.cn>
2018-11-27 20:19:11 +0800

    11-27-Updtae

--- RL/A3C/a3c_keras.py
+++ RL/A3C/a3c_keras.py
@@ -5,8 +5,8 @@ from keras.layers import *
 from keras import backend as K
 import tensorflow as tf 
 #-- constants
-# ENV = 'CartPole-v0'
-ENV = 'Breakout-v0'
+ENV = 'CartPole-v0'
+# ENV = 'Breakout-v0'
 
 RUN_TIME = 30
 THREADS = 8
@@ -234,7 +234,7 @@ class Environment(threading.Thread):
 
 			if done or self.stop_signal:
 				break
-		env.render()
+		# env.render()
 		print("Total R:", R)
 
 	def run(self):
@@ -247,7 +247,6 @@ class Environment(threading.Thread):
 #---------
 class Optimizer(threading.Thread):
 	stop_signal = False
-
 	def __init__(self):
 		threading.Thread.__init__(self)
 
