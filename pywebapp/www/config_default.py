913ad99c5a03a2587355a06893d68c7826bbd554
LouisYZK <yangzhikai@cug.edu.cn>
2019-01-15 21:46:09 +0800

    01-15

---
+++ pywebapp/www/config_default.py
@@ -0,0 +1,17 @@
+'''
+Default configurations
+'''
+
+configs = {
+	'debug': True,
+	'db': {
+		'host': '127.0.0.1',
+		'port': 3306,
+		'user': 'root',
+		'password': 'yangzhikai668',
+		'db': 'awesome'
+	},
+	'session': {
+		'secret': 'Awesome'
+	}
+}
