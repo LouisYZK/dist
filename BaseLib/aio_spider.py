b084955614ae4c89cc945b747d5e6291ff257f4b
LouisYZK <yangzhikai@cug.edu.cn>
2019-01-28 22:47:15 +0800

    1-28-Update

--- BaseLib/aio_spider.py
+++ BaseLib/aio_spider.py
@@ -2,92 +2,93 @@
 import asyncio
 import aiohttp
 import aiomysql
-import re 
+import re
 from pyquery import PyQuery
 
-start_url = 'http://blog.jobbole.com/all-posts/'
+start_url = 'http://blog.jobbole.com/'
 waiting_urls = []
 seen_urls = set()
+seen_titles = set()
 stopping = False
 
-sem = asyncio.Semaphore(5) 
+sem = asyncio.Semaphore(5)
 headers = {
-	'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'
 }
 
 async def fetch(url, session):
-	async with sem:
-		try:
-			async with session.get(url) as resp:
-				# print(resp.status)
-				print('url_status:{}'.format(resp.status))
-				if resp.status in [200, 201]:
-					data = await resp.text()
-					return data
-		except Exception as e:
-				print(e)
+    async with sem:
+    	try:
+    		async with session.get(url) as resp:
+    			# print(resp.status)
+    			print('url_status:{}'.format(resp.status))
+    			if resp.status in [200, 201]:
+    				data = await resp.text()
+    				return data
+    	except Exception as e:
+    			print(e)
+
 
 def extract_urls(html):
-	urls = []
-	pq = PyQuery(html)
-	for link in pq.items('a'):
-		url = link.attr('href')
-		if url and url.startswith('http') and url not in seen_urls:
-			urls.append(url)
-			waiting_urls.append(url)
-	return urls
+    urls = []
+    pq = PyQuery(html)
+    for link in pq.items('a'):
+        url = link.attr('href')
+        if url and url.startswith('http') and url not in seen_urls:
+            urls.append(url)
+            waiting_urls.append(url)
+    return urls
 
 async def init_urls(session):
 	html = await fetch(start_url, session)
 	extract_urls(html)
 
 async def article_handler(url, session, pool):
-	html = await fetch(url, session)
-	seen_urls.add(url)
-	extract_urls(html)
-	try:
-		pq = PyQuery(html)
-		title = pq('title').text()
-		print(url,'.........',title)
-	except Exception as e:
-		print(e)
-	# async with pool.acquire() as conn:
-    #     async with conn.cursor() as cur:
-    #         await cur.execute("INSERT INTO ")
-    #         print(cur.description)
-    #         (r,) = await cur.fetchone()
-    #         assert r == 42
-    # pool.close()
-    # await pool.wait_closed()
+    html = await fetch(url, session)
+    seen_urls.add(url)
+    extract_urls(html)
+    try:
+        pq = PyQuery(html)
+        title = pq('title').text()
+        if title not in seen_titles:
+            seen_titles.add(title)
+            async with pool.acquire() as conn:
+                async with conn.cursor() as cur:
+                    await cur.execute(f"INSERT INTO aio_test(title, url) values('{title}','{url}')")
+                    print(cur.description)
+                    print(title, '已经入库！！！')
+        await pool.wait_closed()
+    except Exception as e:
+        print(e)
 
-async def consumer(pool, session):
+
+async def consumer(pool):
 	# while not stopping:
-	i = 0
-	while i < 50:
-		if len(waiting_urls) == 0:
-			await asyncio.sleep(0.5)
-			continue
-		url = waiting_urls.pop()
-		print('start get url:{}'.format(url))
-		i += 1
-		if re.match(r'http://.*?jobbole.com/\d+/', url):
-			if url not in seen_urls:
-				asyncio.ensure_future(article_handler(url, session, pool))
-				# asyncio.create_task(article_handler(url, session, pool))
+    async with aiohttp.ClientSession(headers = headers) as session:
+        while True:
+            if len(waiting_urls) == 0:
+                await asyncio.sleep(0.5)
+                continue
+            url = waiting_urls.pop()
+            print('start get url:{}'.format(url))
+            if re.match(r'http://blog.jobbole.com/\d+/.*?', url):
+                if url not in seen_urls:
+                    asyncio.ensure_future(article_handler(url, session, pool))
+                    # asyncio.create_task(article_handler(url, session, pool))
+                    await asyncio.sleep(0.5)
 
 async def main(loop):
 	# wait connect building with mysql
-	pool = await aiomysql.create_pool(host='127.0.0.1', port=3306,
-									user='root', password='yangzhikai668',
-									db='spider', loop=loop, charset='utf8',
-									autocommit=True)
+    pool = await aiomysql.create_pool(host='127.0.0.1', port=3306,
+									  user='root', password='yangzhikai668',
+									  db='spider', loop=loop, charset='utf8',
+									  autocommit=True)
 
-	async with aiohttp.ClientSession(headers = headers) as session:
-		html = await fetch(start_url, session)
-		seen_urls.add(start_url)
-		extract_urls(html)
-		# asyncio.ensure_future(consumer(pool, session))
-		asyncio.create_task(consumer(pool, session))
+    async with aiohttp.ClientSession(headers=headers) as session:
+        await init_urls(session)
+        seen_urls.add(start_url)
+
+    asyncio.create_task(consumer(pool))
 
 if __name__ == '__main__':
 	loop = asyncio.get_event_loop()
--- pywebapp/www/app.py
+++ pywebapp/www/app.py
@@ -15,7 +15,7 @@ from jinja2 import Environment, FileSystemLoader
 
 import orm
 from coroweb import add_routes, add_static
-
+from handlers import cookie2user, COOKIE_NAME
 logging.basicConfig(level=logging.INFO)
 
 
@@ -82,6 +82,7 @@ async def response_factory(app, handler):
                 resp.content_type = 'application/json;charset=utf-8'
                 return resp
             else:
+                r['__user__'] = request.__user__
                 resp = web.Response(body=app['__templating__'].get_template(template).render(**r).encode('utf-8'))
                 resp.content_type = 'text/html;charset=utf-8'
                 return resp
@@ -97,6 +98,20 @@ async def response_factory(app, handler):
         return resp
     return response
 
+async def auth_factory(app, handler):
+    async def auth(request):
+        logging.info('check user: %s %s' % (request.method, request.path))
+        request.__user__ = None
+        cookie_str = request.cookies.get(COOKIE_NAME)
+        if cookie_str:
+            user = await cookie2user(cookie_str)
+            if user:
+                logging.info('set current user: %s' % user.email)
+                request.__user__ = user
+        if request.path.startswith('/manage/') and (request.__user__ is None or not request.__user__.admin):
+            return web.HTTPFound('/signin')
+        return (await handler(request))
+    return auth
 
 def datetime_filter(t):
     delta = int(time.time() - t)
@@ -119,7 +134,7 @@ async def init(loop):
                           password='yangzhikai668',
                           db='awesome')
     app = web.Application(loop=loop, middlewares=[
-                        logger_factory, response_factory
+                        logger_factory, response_factory, auth_factory
                         ])
     init_jinja2(app, filters=dict(datetime=datetime_filter))
     add_routes(app, 'handlers')
